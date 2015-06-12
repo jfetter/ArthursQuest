46036ce4ede3ef1ceb9324351310faa031a3e19e
jfetter <jillian.fetter@gmail.com>
2015-06-11 21:18:44 -0700

    mostly styled and pretty solid html, but script needs work still.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,37 @@
-fill in blanks
+
 $(document).ready(function(){
-  $(".answer").hide);
-  $("#q[i]").show(function(key, userAnswers){
+  $(".fillIn").hide();
+});//document ready
+var key =["<b>bo</b>red","penises","ex-boyfriend","pi","darth vader","children","tickled","senior citizen","preforming","lolos","ever","sand bar", "came" ];
+var userAnswers =[];
 
-  });
-});
+var pushToUserAnswers = function(userAnswers){
+  var input = document.getElementById("answer[i]").value;
+  var correct = userAnswers.push(input).toLowerCase();
+  return correct;
+};
 
-var input = document.getElementById("q[i]").value;
-var key =[""];
-var userAnswers =[""];
-var fillIn=[""]
+var replaceInputWithCorrectAnswer = function()
+{
+//change the display so that it removes the input box and replaces it with the answer. Bold the letters that are being used for the sub answer.
+};
+
+var checkAnswers = function(){
 
 for (var i=0; i<key.length; i++)
 {
-
+//if wrong twice just give the answer...
 if (key[i]=== userAnswers[i])
 {
-  userAnswers.push(input[i]);
-  document.getElementById("fillIn").innerHTML = fillIn[i];
+  replaceInputWithCorrectAnswer;
+  pushToUserAnswers();
+  $("#p[i+1]").show;
   }
 }
 else {
-"wrong  + pic of laughing dog from duck hunt."
-}
+  userAnswers.pop();
+i --;
+};
+}//for
 return false;
 }; //function
